dd4f3a07e5ac830da46adf068ad3bbe2560ca813
Brian C <283823263+reisntea@users.noreply.github.com>
2026-07-12 20:03:42 -0400

    Fix logic with draws and moved the winner check
    Draws were calculated before any wins were checked so if the winning
    move was on the ninth turn it would say draw. Also moved the winner
    check to be in the gameController so that scores would be easier to
    calculate.

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
+// Represents the state of the game
+// It first creates a 3x3 2d array and fills each with a cell.
 function Gameboard () {
     const rows = 3;
     const columns = 3;
     const board = [];
-    let turns = 0;
 
     for (let i = 0; i < rows; i++) {
         board[i] = [];
@@ -11,6 +12,11 @@ function Gameboard () {
         }
     }
 
+    const getBoard = () => board;
+
+    // The checkCell variable is to make sure that the move is valid before doing anything.
+    // Returns false if the move was invalid and true if it was.
+    // Each time a valid move is made it checks if that was a winning move.
     const makeMark = (row, column, playerMark) => {
         const checkCell = board[row][column].getValue() === 0 ? true : false;
 
@@ -20,38 +26,7 @@ function Gameboard () {
         }
 
         board[row][column].addMark(playerMark);
-        turns++;
-        checkForWinner(row, column, playerMark);
         return true;
-    };
-
-    const checkForWinner = (row, column, playerMark) => {
-        if (turns == 9) {
-            console.log("draw");
-        }
-
-        // First it checks the row where the mark was placed, then it checks it's column, and then it checks both diagonals. 
-        // currentRow is used to differentiate from the row parameter.
-        switch (true) {
-            case (board[row].every((space) => space.getValue() === playerMark)):
-                playerMark === "X" ? console.log(`Player 1 wins`) : console.log(`Player 2 wins`);
-                console.log("case 1");
-                break;
-            case (board.map((currentRow) => currentRow[column]).every((space) => space.getValue() === playerMark)):
-                playerMark === "X" ? console.log(`Player 1 wins`) : console.log(`Player 2 wins`);
-                console.log("case 2");
-                break;
-            case (board[0][0].getValue() === playerMark && board[1][1].getValue() === playerMark && board[2][2].getValue() === playerMark):
-                playerMark === "X" ? console.log(`Player 1 wins`) : console.log(`Player 2 wins`);
-                console.log("case 3");
-                break;
-            case (board[2][0].getValue() === playerMark && board[1][1].getValue() === playerMark && board[0][2].getValue() === playerMark):
-                playerMark === "X" ? console.log(`Player 1 wins`) : console.log(`Player 2 wins`);
-                console.log("case 4");
-                break;
-            default:
-                console.log("no winner");
-        }
     };
 
     const printBoard = () => {
@@ -62,16 +37,21 @@ function Gameboard () {
     return {
         makeMark,
         printBoard,
+        getBoard,
     }
 }
 
+// Each cell represents one square on the board
+// A cell can have either 0, meaning unfilled, or X / O, meaning marked by either player.
 function Cell () {
     let value = 0;
 
+    // Marks the cell with the mark of the player whose turn it is.
     const addMark = (playerMark) => {
         value = playerMark;
     };
 
+    // Returns the value of the cell.
     const getValue = () => value;
 
     return {
@@ -80,8 +60,11 @@ function Cell () {
     };
 }
 
+// Controls the data for each player and whose turn it is.
+// Also checks if someone makes a winning move.
 function GameController (playerOneName = "Player One", playerTwoName = "Player Two") {
     const board = Gameboard();
+    let turns = 0;
 
     const players = [
         {
@@ -111,9 +94,44 @@ function GameController (playerOneName = "Player One", playerTwoName = "Player T
             return;
         }
 
+        turns++;
+        checkForWinner(row, column);
         switchPlayerTurn();
         printNewRound();
-    }
+    };
+
+
+    const checkForWinner = (row, column) => {
+        // First it checks the row where the mark was placed, then it checks it's column, and then it checks both diagonals. 
+        // currentRow is used to differentiate from the row parameter.
+        switch (true) {
+            case (board.getBoard()[row].every((space) => space.getValue() === getActivePlayer().mark)):
+                getActivePlayer() === players[0] ? console.log(`Player 1 wins`) : console.log(`Player 2 wins`);
+                console.log("case 1");
+                break;
+            case (board.getBoard().map((currentRow) => currentRow[column]).every((space) => space.getValue() === getActivePlayer().mark)):
+                getActivePlayer() === players[0] ? console.log(`Player 1 wins`) : console.log(`Player 2 wins`);
+                console.log("case 2");
+                break;
+            case (board.getBoard()[0][0].getValue() === getActivePlayer().mark && board.getBoard()[1][1].getValue() === getActivePlayer().mark && board.getBoard()[2][2].getValue() === getActivePlayer().mark):
+                getActivePlayer() === players[0] ? console.log(`Player 1 wins`) : console.log(`Player 2 wins`);
+                console.log("case 3");
+                break;
+            case (board.getBoard()[2][0].getValue() === getActivePlayer().mark && board.getBoard()[1][1].getValue() === getActivePlayer().mark && board.getBoard()[0][2].getValue() === getActivePlayer().mark):
+                getActivePlayer() === players[0] ? console.log(`Player 1 wins`) : console.log(`Player 2 wins`);
+                console.log("case 4");
+                break;
+            default:
+                console.log("no winner");
+        }
+
+        // If 9 turns pass that means the board is completely filled
+        // So if there's no winner after nine turns it means a draw
+        if (turns == 9) {
+            console.log("draw");
+        }
+    };
+
 
     const printNewRound = () => {
         board.printBoard();
